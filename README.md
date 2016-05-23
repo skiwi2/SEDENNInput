546462a2cec0a38ce2823def4dc8353ad09838bf
skiwi <frank_skiwi@hotmail.com>
2016-05-23 20:10:37 +0200

    Added GloVe lookup example, updated readme.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SEDENNInput
 Converter to prepare input for a neural network.
+
+## Running the program
+
+First to get the GloVe dataset you need to download http://nlp.stanford.edu/data/glove.6B.zip and extract it to D:\Downloads. Secondly you need to make sure that you use `-Djava.library.path=""` in your JVM options when running this program.
+
+It also may take a minute before a result is given due to loading the big dataset.
---
+++ src/main/java/com/skiwi/sedenninput/GloveMain.java
@@ -0,0 +1,28 @@
+package com.skiwi.sedenninput;
+
+import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
+import org.deeplearning4j.models.embeddings.wordvectors.WordVectors;
+
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+/**
+ * @author Frank van Heeswijk
+ */
+public class GloveMain {
+    private void init() throws FileNotFoundException, UnsupportedEncodingException {
+        WordVectors wordVectors = WordVectorSerializer.loadTxtVectors(Paths.get("D:\\Downloads\\glove.6B\\glove.6B.300d.txt").toFile());
+
+        //prints the 300 length vector associated with the word "cat"
+        System.out.println(Arrays.toString(wordVectors.getWordVector("cat")));
+
+        //prints the 10 closest words to "cat"
+        System.out.println(wordVectors.wordsNearest("cat", 10));
+    }
+
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+        new GloveMain().init();
+    }
+}
